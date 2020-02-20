f7b8c190a9f46907edb914d3ef405679c208b3b8
Teo <teodor.patras@gmail.com>
2020-02-20 17:36:04 +0100

    Add code folding

--- src/content.js
+++ src/content.js
@@ -1,12 +1,71 @@
-const { href } = document.location;
+const parseURL = url => {
+  const parser = document.createElement("a");
+  const searchObject = {};
+  // Let the browser do the work
+  parser.href = url;
+  // Convert query string to object
+  const queries = parser.search.replace(/^\?/, "").split("&");
+  for (let i = 0; i < queries.length; i++) {
+    const split = queries[i].split("=");
+    searchObject[split[0]] = split[1];
+  }
+  return {
+    protocol: parser.protocol,
+    host: parser.host,
+    hostname: parser.hostname,
+    port: parser.port,
+    pathname: parser.pathname,
+    search: parser.search,
+    searchObject: searchObject,
+    hash: parser.hash
+  };
+};
 
-if (href.endsWith(".yaml") || href.endsWith(".yml")) {
-  const myCodeMirror = CodeMirror(document.body, {
+const getMode = () => {
+  const modes = {
+    yaml: ["yaml", "yml"],
+    python: ["py"],
+    javascript: ["js", "ts", "json"],
+    jsx: ["jsx"],
+    json: ["json"],
+    xml: ["xml"],
+    markdown: ["markdown", "md"]
+  };
+
+  const { pathname } = parseURL(document.location.href);
+  const extension = /[.]/.exec(pathname)
+    ? /[^.]+$/.exec(pathname)[0].toLowerCase()
+    : null;
+
+  for (const mode in modes) {
+    if (modes[mode].includes(extension)) {
+      if (extension === "json") {
+        // special setting for json
+        return { name: "javascript", json: true };
+      }
+      return mode;
+    }
+  }
+  return null;
+};
+
+const mode = getMode();
+
+if (mode && document.body.childNodes.length === 1) {
+  const config = {
     value: document.body.firstChild.textContent,
-    mode: "yaml",
     lineNumbers: true,
+    foldGutter: true,
+    lineWrapping: true,
     readOnly: true,
-    scrollbarStyle: null
+    scrollbarStyle: null,
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
+    theme: "neo"
+  };
+
+  window.codeMirror = CodeMirror(document.body, {
+    mode,
+    ...config
   });
   document.body.firstChild.remove();
   document.body.firstChild.style.height = "100vh";
